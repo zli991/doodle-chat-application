a830190ec36d7cb3f875d161f1593642a980b25b
Ilic Ivan <Ivan.Ilic@swisscom.com>
2022-04-20 20:07:55 +0200

    initial commit

---
+++ src/main/java/com/doodle/DoodleChatApplication.java
@@ -0,0 +1,12 @@
+package com.doodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoodleChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoodleChatApplication.class, args);
+    }
+}
