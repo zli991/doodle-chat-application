c39fd02f825e6bb40269ff1b2aac1ef4c316aaaa
Ilic Ivan <Ivan.Ilic@swisscom.com>
2022-04-26 23:03:38 +0200

    swagger annotations and Message model refactoring

--- src/main/java/com/doodle/controller/MessageController.java
+++ src/main/java/com/doodle/controller/MessageController.java
@@ -4,6 +4,7 @@ import com.doodle.dto.MessageSearchOptions;
 import com.doodle.dto.MessageSearchResponse;
 import com.doodle.model.Message;
 import com.doodle.service.MessageService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,11 +20,13 @@ public class MessageController {
     }
 
     @PostMapping("/search")
+    @Operation(summary = "Returns page with number of results provided in search options. If no options provided, page with first 5 messages will be returned")
     public Page<MessageSearchResponse> findAll(@RequestBody(required = false) final MessageSearchOptions options) {
         return messageService.findAll(options);
     }
 
     @PostMapping
+    @Operation(summary = "Persists new message, and returns saved entry.")
     public Message save(@RequestBody final Message message) {
         return messageService.save(message);
     }
--- src/main/java/com/doodle/model/Message.java
+++ src/main/java/com/doodle/model/Message.java
@@ -1,36 +1,28 @@
 package com.doodle.model;
 
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoId;
 
 import java.time.LocalDateTime;
 
 @Document
 public class Message {
 
-    @MongoId
-    private String id;
     private String text;
     private String creator;
-    private LocalDateTime createdOn;
+    private final LocalDateTime createdOn;
 
     public Message() {
         this.createdOn = LocalDateTime.now();
     }
 
     public Message(final String text, final String creator) {
-        this();
-        this.text = text;
-        this.creator = creator;
+        this(text, creator, LocalDateTime.now());
     }
 
     public Message(final String text, final String creator, final LocalDateTime createdOn) {
-        this(text, creator);
+        this.text = text;
+        this.creator = creator;
         this.createdOn = createdOn;
-    }
-
-    public String getId() {
-        return id;
     }
 
     public String getText() {
